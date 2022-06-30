2a50ef9dce802dd9cfd1d3df2880fd2c1d51eb91
MB <michael.dburke56@gmail.com>
2022-06-30 16:03:00 -0500

    Write unit test for gas report purposes.

--- test/Contract.test.js
+++ test/Contract.test.js
@@ -1,18 +1,21 @@
-const { expect } = require('chai');
+const { expect, use } = require('chai');
 const { ethers } = require('hardhat');
+
+use(require('chai-as-promised'));
 
 describe('Contract', function () {
   before(async function () {
-    //this.Contract = await ethers.getContractFactory('Contract');
+    this.Contract = await ethers.getContractFactory('Contract');
   });
 
   beforeEach(async function () {
-    //this.contract = await this.Contract.deploy();
-    //await this.contract.deployed();
+    this.contract = await this.Contract.deploy();
+    await this.contract.deployed();
   });
 
-  it('test descriptor', async function () {
-    expect('2').to.equal('2');
+  it('should mint without error', async function () {
+      //Non functional test written ONLY to display gas requriements. 
+      await this.contract.mint();
+      expect(true).to.equal(true);
   });
-  
 });
